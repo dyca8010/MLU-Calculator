6789d3d96539a9f677089e7aa65fdd57f891fa16
dyca8010 <dyca8010@colorado.edu>
2018-01-28 16:59:07 -0700

    Update MLUCalculator.py

--- MLUCalculator.py
+++ MLUCalculator.py
@@ -1,12 +1,12 @@
 #MLU Calculation Code
 #Dylan Cassady
 #University of Colorado Boulder
-#Spring 2018
+#Fall 2017
 
 import re;
 
 
-T = "Text. Goes. Here."
+T = "Bacon ipsum's dolor amet pork bresaola hamburger, ham hock biltong drumstick andouille kielbasa sausage beef ribs chicken tenderloin. Ball tip pork jowl prosciutto t-bone, bresaola andouille tenderloin beef shank tail leberkas. Shoulder porchetta picanha, turducken bacon spare ribs beef ribs leberkas frankfurter flank short loin salami. Ribeye kevin beef ribs corned beef shankle shoulder shank burgdoggen pork loin. Cow leberkas beef ribs tenderloin. Landjaeger shank kevin, doner prosciutto kielbasa pancetta."
 
 
 def separate(txt):
@@ -23,9 +23,15 @@ def separate(txt):
 		elif letter == "." and txt[wStart:(wEnd)] != "":
 			wordArray.append(txt[wStart:(wEnd)])
 			wStart = wEnd + 2
+		elif letter == "!" or letter == "?" and txt[wStart:(wEnd)] != "":
+			wordArray.append(txt[wStart:(wEnd)])
+			wStart = wEnd + 2	
+			
 		wEnd = wEnd + 1
 	if wStart != wEnd and txt[wStart:(wEnd)] != "":
 		wordArray.append(txt[wStart:(wEnd)])
+	print wordArray
+	print ""
 	print "Word Count:"
 	print len(wordArray)
 	return wordArray
@@ -34,7 +40,7 @@ def sentenceCount(txt,wordArray):
 	count = 0
 	ii = 0
 	for letter in txt:
-		if letter == ".":
+		if letter == "." or letter == "?" or letter == "!":
 			count = count + 1
 		ii = ii + 1
 	print ""
@@ -43,21 +49,18 @@ def sentenceCount(txt,wordArray):
 	return count
 
 def prefixMorpheme(txt):
-	match = re.search('^un', txt)
-	if match:
+	pref1 = re.search('^un', txt)
+	pref2 = re.search('^dis',txt)
+	if pref1 or pref2:
 		return 1
 	else:
 		return 0
 	
 def suffixMorpheme(txt):
-	match1 = re.search("s$", txt)
-	match2 = re.search("ed$", txt)
-	match3 = re.search("ing$", txt)
-	if match1:
-		return 1
-	elif match2:
-		return 1
-	elif match3:
+	suf1 = re.search("s$", txt)
+	suf2 = re.search("ed$", txt)
+	suf3 = re.search("ing$", txt)
+	if suf1 or suf2 or suf3:
 		return 1
 	else:
 		return 0
@@ -75,11 +78,14 @@ def MLUCalculator(txt):
 	print "MorphCount:"
 	print morphCount
 	print ""
-	MLU = float(morphCount)/float(sCount)
+	if sCount != 0:
+		MLU = float(morphCount)/float(sCount)
+	else:
+		MLU = "undefined"
+		print "Please end all uterances with appropriate puctuation."
 	return MLU
 
 MLU = MLUCalculator(T)
 print "MLU:"
 print MLU
-
 
