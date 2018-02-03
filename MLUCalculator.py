8822cbb2257d7739ca774241b77e108d9a2fd6a3
dyca8010 <dyca8010@colorado.edu>
2018-02-03 12:40:42 -0700

    Update MLUCalculator.py

--- MLUCalculator.py
+++ MLUCalculator.py
@@ -5,18 +5,17 @@
 
 import re;
 
+#Text to analyze goes here
+T = "Bacon ipsum dolor amet alcatra doner bresaola sirloin ham kevin shankle tenderloin boudin jowl beef ribs jerky spare ribs short ribs. Meatball ham hock pork loin sirloin pancetta turducken. Strip steak pig capicola prosciutto pork chop jowl. Sausage pork buffalo turducken flank, shoulder rump kevin. Shankle ground round shank leberkas, spare ribs kielbasa filet mignon tri-tip alcatra sausage pork doner pork belly chicken. Tri-tip ribeye bresaola filet mignon buffalo turkey, beef ribs short loin ham hock beef cow cupim hamburger tongue. Meatball ham hock ribeye, turkey kielbasa kevin bacon pig pork t-bone beef spare ribs hamburger shankle."
 
-T = "Bacon ipsum's dolor amet pork bresaola hamburger, ham hock biltong drumstick andouille kielbasa sausage beef ribs chicken tenderloin. Ball tip pork jowl prosciutto t-bone, bresaola andouille tenderloin beef shank tail leberkas. Shoulder porchetta picanha, turducken bacon spare ribs beef ribs leberkas frankfurter flank short loin salami. Ribeye kevin beef ribs corned beef shankle shoulder shank burgdoggen pork loin. Cow leberkas beef ribs tenderloin. Landjaeger shank kevin, doner prosciutto kielbasa pancetta."
-
-
-def separate(txt):
+def separate(txt): #break the paragraph into individual words, all lowercase
 	wStart = 0
 	wEnd = 0
 	wordArray = []
 	for letter in txt:
 		if letter == "," and txt[wStart:(wEnd)] != "":
 			wordArray.append(txt[wStart:(wEnd)])
-			wStart = wEnd + 2		
+			wStart = wEnd + 2
 		elif letter == " " and txt[wStart:(wEnd)] != "":
 			wordArray.append(txt[wStart:(wEnd)])
 			wStart = wEnd + 1
@@ -25,43 +24,47 @@ def separate(txt):
 			wStart = wEnd + 2
 		elif letter == "!" or letter == "?" and txt[wStart:(wEnd)] != "":
 			wordArray.append(txt[wStart:(wEnd)])
-			wStart = wEnd + 2	
-			
+			wStart = wEnd + 2
+
 		wEnd = wEnd + 1
 	if wStart != wEnd and txt[wStart:(wEnd)] != "":
 		wordArray.append(txt[wStart:(wEnd)])
-	print wordArray
-	print ""
-	print "Word Count:"
-	print len(wordArray)
+	#print (wordArray)
+	#print ()
+	print ("Word Count:")
+	print (len(wordArray))
 	return wordArray
 
-def sentenceCount(txt,wordArray):
+def sentenceCount(txt,wordArray): #count the number of sentences using punctuation
 	count = 0
 	ii = 0
 	for letter in txt:
 		if letter == "." or letter == "?" or letter == "!":
 			count = count + 1
 		ii = ii + 1
-	print ""
-	print "Sentence Count:"
-	print count
+	print ()
+	print ("Utterance Count:")
+	print (count)
 	return count
 
 def prefixMorpheme(txt):
-	pref1 = re.search('^un', txt)
 	pref2 = re.search('^dis',txt)
-	if pref1 or pref2:
+	if pref2:
+		#print (txt) #prints each word flagged as having a prefix
 		return 1
 	else:
 		return 0
-	
-def suffixMorpheme(txt):
+
+def suffixMorpheme(txt): #looks for words that end in s, ed, or ing
 	suf1 = re.search("s$", txt)
 	suf2 = re.search("ed$", txt)
 	suf3 = re.search("ing$", txt)
 	if suf1 or suf2 or suf3:
-		return 1
+		if txt == "was" or txt == "is" or txt == "boing" or txt == "this" or txt == "being" or txt == "has" or txt == "oops" or txt == "thing":
+			return 0
+		else:
+			#print (txt) #prints each word flagged as having a suffix
+			return 1
 	else:
 		return 0
 
@@ -74,18 +77,21 @@ def MLUCalculator(txt):
 		morphCount = morphCount + prefixMorpheme(word)
 	for word2 in txtArray:
 		morphCount = morphCount + suffixMorpheme(word2)
-	print ""
-	print "MorphCount:"
-	print morphCount
-	print ""
+	print ()
+	print ("MorphCount:")
+	print (morphCount)
+	print ()
 	if sCount != 0:
 		MLU = float(morphCount)/float(sCount)
 	else:
-		MLU = "undefined"
-		print "Please end all uterances with appropriate puctuation."
+		MLU = ("undefined")
+		print ("Please end all uterances with appropriate puctuation.")
 	return MLU
 
 MLU = MLUCalculator(T)
-print "MLU:"
-print MLU
+print ("MLU:")
+print (MLU)
 
+def wordTypes(txt):
+	txt=txt.lower
+	txtArray = separate(txt)
